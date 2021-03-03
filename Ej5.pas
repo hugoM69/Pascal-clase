8f2ca8fc1b9f2a5077208cc420cdabec64d3c7f4
hugoM69 <64667618+hugoM69@users.noreply.github.com>
2021-03-03 12:56:42 +0100

    Update Ej5.pas

--- Ej5.pas
+++ Ej5.pas
@@ -5,11 +5,11 @@ BEGIN
 	READLN(N);
 	READLN(Epsilon);
 	READLN(r0);
-	r1:= ((N/r0)+r0)/2;
-	WHILE NOT (ABS(r1-r0)<= Epsilon) DO BEGIN
+	r1:= r0;
+	REPEAT  BEGIN
 		r0 := r1;
 		r1 := ((N/r0)+r0)/2;
-	END;
-	WRITELN('La raiz vale:  ',r1);
-	
+		END;
+	UNTIL(ABS(r1-r0)<= Epsilon);
+	WRITELN('La raiz vale:  ',r1);  
 END.
